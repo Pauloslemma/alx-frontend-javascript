e048b61c09e64913921067bd832646e8dd464f0f
pauloslemma <pauloslemma1994@gmail.com>
2024-03-08 22:56:47 +0300

    first commit

---
+++ 0x00-ES6_basic/babel.config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+	rts = {
+		  presets: [
+			      [
+				            '@babel/preset-env',
+				            {
+						            targets: {
+								              node: 'current',
+								            },
+						          },
+				          ],
+			    ],
+	};
+
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
