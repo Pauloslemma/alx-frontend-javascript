f9ec0800bf0e99b820d853dc6412a564a99299fa
pauloslemma <pauloslemma1994@gmail.com>
2024-03-09 00:31:35 +0300

    updated

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,24 @@
+//1-block-scoped.js
+//rt default function taskBlock(trueOrFalse) {
+//	    let task = false; // Use let instead of var
+//	    	    let task2 = true; // Use let instead of var
+//	    	    	  
+//	    	    	  	    if (trueOrFalse) {
+//	    	    	  	    		          task = true; // Do not use var here
+//	    	    	  	    		          		          task2 = false; // Do not use var here
+//	    	    	  	    		          		          		        }
+//	    	    	  	    		          		          		        	  
+//	    	    	  	    		          		          		        	  	    return [task, task2];
+//	    	    	  	    		          		          		        	  	    	  }
+
+export default function taskBlock(trueOrFalse) {
+	    let task = false; // Use let instead of var
+	    let task2 = true; // Use let instead of var
+	  
+	    if (trueOrFalse) {
+		          task = true; // Do not use var here
+		          task2 = false; // Do not use var here
+		        }
+	  
+	    return [task, task2];
+	  }
